4111ce45b2444f8d34164debe54e1af1834ee097
REMILA RAFIK <rafik.remila@gmail.com>
2026-03-12 14:52:22 +0000

    auto-commit for 4ebac1e9-9a90-4e98-aad2-85cd2a5447dc

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Depends, UploadFile, File, status, Request, Body
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, UploadFile, File, status, Request, Body, Query
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
